bb0a348aea036bdf0a537a33bce970dd648cc07f
drkomp <drkomp333@gmail.com>
2017-03-14 00:03:49 +0200

    opt

--- src/SearchingPalindrome.java
+++ src/SearchingPalindrome.java
@@ -3,15 +3,18 @@
  */
 public class SearchingPalindrome {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        int iterations = 0;
         System.out.println("Searching maximal palindrome number produced by multiplication of two 3 digits number...");
         SearchingPalindrome spInstance = new SearchingPalindrome();
         int maxNumOne=0;
         int maxNumTwo=0;
         int maxPalindrome = 0;
         int currentProduction;
-        for(int numOne = 999; numOne >0;numOne--){
-            for(int numTwo = 999;numTwo >0;numTwo--){
+        for(int numOne = 999; numOne >99;numOne--){
+            for(int numTwo = 999;numTwo >99;numTwo--){
                 currentProduction = numOne*numTwo;
+                iterations++;
                 if(spInstance.isPalindrome(currentProduction)){
                     if(maxPalindrome < currentProduction){
                         maxPalindrome = currentProduction;
@@ -24,17 +27,14 @@ public class SearchingPalindrome {
         System.out.println("Maximum palindrome number is: " + maxPalindrome);
         System.out.println("First multiplier is: " + maxNumOne);
         System.out.println("Second multiplier is: " + maxNumTwo);
+        System.out.println("Time: "+(System.currentTimeMillis() - startTime));
+        System.out.println("Iterations: "+ iterations);
     }
 
     boolean isPalindrome(int SomeNumber){
         if (SomeNumber < 0 ) {throw new ArithmeticException("Number must be positive");}
-        if (SomeNumber < 10) {return true;}
-        int someNumLength = Integer.toString(SomeNumber).length();
-        String partOneOfSomeNum = Integer.toString(SomeNumber).substring(0,someNumLength >>> 1);
-        String partTwoOfSomeNum = new StringBuffer(Integer.toString(SomeNumber).substring((someNumLength >>> 1) + (someNumLength & 1))).reverse().toString();
-        return partOneOfSomeNum.equals(partTwoOfSomeNum);
-        //someNumLength >>> 1 is equivalent someNumLength/2
-        //someNumLength & 1 is equivalent someNumLength%2
-
+        return Integer.toString(SomeNumber).equals(new StringBuffer(Integer.toString(SomeNumber)).reverse().toString());
     }
+
+
 }
---
+++ src/Searching_two.java
@@ -0,0 +1,8 @@
+/**
+ * Created by drkomp on 13.03.2017.
+ */
+public class Searching_two {
+    public static void main(String[] args) {
+
+    }
+}
